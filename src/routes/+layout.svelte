ca10d24dbb42ce6d98fe7dd2e523d300546d737b
ysk3a <53593899+ysk3a@users.noreply.github.com>
2023-08-11 02:08:22 -0400

    explore adding my components, some skeleton.dev components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,27 @@
-<script lang='ts'>
+<script lang="ts">
+	export const ssr = false;
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	import { LightSwitch, AppShell } from '@skeletonlabs/skeleton';
+	import type { ComponentEvents } from 'svelte';
+
+	function scrollHandler(event: ComponentEvents<AppShell>['scroll']) {
+		console.log(event.currentTarget.scrollTop);
+	}
 </script>
 
-<slot />
+<AppShell on:scroll={scrollHandler}>
+	<svelte:fragment slot="header"><LightSwitch /></svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,21 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import quizzes from '../data.json';
+	// import * as images from '../image';
+	import Card from '../components/Card.svelte';
+	// const access = (str: string) => {
+	// 	return images[str as keyof typeof images];
+	// };
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-		</ul>
-	</div>
+	{#each quizzes as quiz (quiz.id)}
+		<Card {quiz} />
+		<!-- <div class="quiz-card shadow-lg shadow-blue-500/50 hover:shadow-indigo-500/50 w-[250px] rounded overflow-hidden cursor-pointer m-2">
+			<img src={access(quiz.imageUrl)} alt="chemistry" class="h-44" />
+			<div class="flex item-center justify-between py-3 px-2">
+				<h1 class="font-bold">{quiz.name}</h1>
+				<p>{quiz.questions.length} questions</p>
+			</div>
+		</div> -->
+	{/each}
 </div>
